3c2a127d7b4ce82e062060c22f21ca819412b542
Giovannalima2607 <giovannasilva123@gmail.com>
2024-09-25 14:17:56 -0300

    first commit

---
+++ atividade 25.py
@@ -0,0 +1,16 @@
+
+
+nota1=float(input("Digite a nota 1: "))
+nota2=float(input("Digite a nota 2: "))
+nota3=float(input("Digite a nota 3: "))
+nota4=float(input("Digite a nota 4: "))
+nota5=float(input("digite a nota 5: "))
+media = (nota1 + nota2 + nota3 + nota4 + nota5) /5
+print("A primeira é: ",nota1)
+print("A segunda é: ",nota2)
+print("A terceira é: ",nota3)
+print("A quarta é: ",nota4)
+print("A quinta é: ",nota5)
+print("A média final é: ",media)
+if media >= 7:
+  print("Aprovado")
